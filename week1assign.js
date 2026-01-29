535eb91d8f40d2572019acf354d8817baa42ca75
LetCodewithPreethi <preethi.smvijayakumar2007@gmail.com>
2026-01-29 06:24:39 +0530

    Add files via upload

---
+++ week1assign.js
@@ -0,0 +1,14 @@
+// Declaring the variables
+let userName = "Preethi";        // String
+let organization = "Preethi";   // String
+let contactNumber = 123456789;  // Number
+let knowsAutomation = true;     // Boolean
+let usesPlaywright;             // Undefined (not assigned)
+
+// Printing values
+console.log("User Name is: " + userName);
+console.log("Organization is: " + organization);
+console.log("Contact Number is: " + contactNumber);
+console.log("Is Automation Proficient: " + knowsAutomation);
+console.log("Uses Playwright: " + usesPlaywright);
+
